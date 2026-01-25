8d269c9ce29f269c5c933f79adce05cb64cdd5e9
wow50000 <48336887+wow50000@users.noreply.github.com>
2026-01-25 03:48:26 +0000

    fixes race locks

--- tools/tgs_test/obj/Debug/net8.0/Tgstation.TgsTest.AssemblyInfo.cs
+++ tools/tgs_test/obj/Debug/net8.0/Tgstation.TgsTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tgstation.TgsTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0+ad089cf7dec55d0f12ab2ffc7253d1471f2f7e39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0+32b2fd8a919e3540778926ebd68def299482732d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tgstation.TgsTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tgstation.TgsTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
